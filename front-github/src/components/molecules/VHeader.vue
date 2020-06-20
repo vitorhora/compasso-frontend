e007b1b2e2b8e42e9c707d34bc0b2fd47d6013df
vitor Cavalcante <vitor.hora@g4f.com.br>
2020-06-20 15:37:03 -0300

    Ajuste SEO.

--- front-github/src/components/molecules/VHeader.vue
+++ front-github/src/components/molecules/VHeader.vue
@@ -11,3 +11,35 @@
 </template>
 
 
+<script>
+ 
+  export default {
+    data: function () {
+      return {
+        title: 'Informações Usuário'
+      }
+    },
+   
+    head: {  
+      title: function () {
+        return {
+          inner: this.title
+        }
+      },
+      meta: [
+        { name: 'description', content: 'Veja gratuitamente informações dos usuários GitHub, repositórios, favoritos com apenas o nome do usuário. O repositório GitHub é onde estão localizados fontes de aplicativos em diversas tecnologias.', id: 'description',  },
+        { name: 'application-name', content: 'Informções GIT' },
+        { name: 'author', content: 'Vitor Cavalcante Hora' },
+        { name: 'keywords', content: 'consulta, github, favoritos, repositório, informações, usuário, git' }
+      ],
+
+      link: [
+        { rel: 'author', href: 'author', undo: false },
+        { rel: 'canonical', href: 'http://localhost:8080' }
+      ]
+    }  
+  }
+  
+</script>
+
+
--- front-github/src/main.js
+++ front-github/src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import VueHead from 'vue-head'
 
 Vue.config.productionTip = false
+
+Vue.use(VueHead)
+Vue.use(VueRouter)
 
 new Vue({
   render: h => h(App)
